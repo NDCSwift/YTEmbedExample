07cbb313c2ef31030fdb2bcf032cbea8e4cad64e
Noah <152039306+NDCSwift@users.noreply.github.com>
2026-06-14 14:50:28 -0400

    Update YouTube handle to @NoahDoesCoding

--- YTEmbedExample2/YTEmbedExample2App.swift
+++ YTEmbedExample2/YTEmbedExample2App.swift
@@ -3,7 +3,7 @@
     //  File: YTEmbedExample2App.swift
     //  Created by Noah Carpenter
     //  🐱 Follow me on YouTube! 🎥
-    //  https://www.youtube.com/@NoahDoesCoding97
+    //  https://www.youtube.com/@NoahDoesCoding
     //  Like and Subscribe for coding tutorials and fun! 💻✨
     //  Fun Fact: Cats have five toes on their front paws, but only four on their back paws! 🐾
     //  Dream Big, Code Bigger
